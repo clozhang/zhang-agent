6ce6dcb1af51ddddbddfb84e0ba0ee43209375eb
Duncan McGreggor <oubiwann@gmail.com>
2017-05-09 20:33:21 -0500

    Bumped to next dev version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojang/zhang-agent "0.1.0-SNAPSHOT"
+(defproject clojang/zhang-agent "0.2.0-SNAPSHOT"
   :description "Zhang Node and REPL Start-up"
   :url "https://github.com/clojang/zhang-agent"
   :scm {
